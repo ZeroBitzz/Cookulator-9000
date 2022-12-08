33c1a8ad6b06a5d24caaac9778c50425cc5b5857
Broderick <zerothesu@gmail.com>
2022-12-08 03:30:46 -0700

    added total integration of recipe API based off of your score you get during game with added measurements as well. Refactored some code in main and index, and added the rest of the page components for the game. Also added day.js as the second api for the app.

--- assests/scripts/main.js
+++ assests/scripts/main.js
@@ -1,11 +1,13 @@
 // IMPORTS
 import { askQuestion } from './askQuestion.js'
 
-
 // EVENT LISTENER TO START THE GAME
 let initialButton = document.getElementById("initial-button")
 initialButton.addEventListener("click", jsGame)
 
+// CURRENT DATE API
+let time = document.getElementById('time')
+time.innerHTML = dayjs().format('MMMM DD YYYY')
 
 // GLOBAL DOM VARIABLES
 let gameSection = document.getElementById("game-content-section")
@@ -43,8 +45,75 @@ function jsGame() {
     askQuestion() // starts asking questions(see askquestion in other file for details)
 }
 
+// the API
+function showRecipe() {
+    let mealCategory
+    let randomMealId
+    const mealTitleElement = document.getElementById("random-recipe-title")
+    const mealIngredientsElement = document.getElementById("random-recipe-ingredients")
+    const recipeInstructionElement = document.getElementById("random-recipe-instruction")
+    const recipeMeasurementsElement = document.getElementById('random-recipe-measurements')
+
+    // TODO: instead of using .createElement(), we should just have the elements already defined in the html
+    let recipeName = document.createElement("h4")
+    let ingredientsElement = document.createElement("h4")
+    let recipeBulk = document.createElement("h5")
+    let measurementsElement = document.createElement("h6")
+
+    // NESTED TERNARY DETERMINING WHICH CATEGORY THE MEAL WILL COME FROM
+    correctAnswers === 8 
+    ? mealCategory = 'Seafood'
+        : (correctAnswers === 7 || correctAnswers === 6) 
+        ? mealCategory = 'Dessert'
+            : (correctAnswers === 5 || correctAnswers === 4) 
+            ? mealCategory = 'Pork'
+                : (correctAnswers === 3 || correctAnswers === 2) 
+                ? mealCategory = 'Vegetarian'
+    : mealCategory = 'Breakfast'
+
+    // FETCH REQUESTS FOR GETTING A RANDOM MEAL BASED ON PREVIOUS SCORE ABOVE WITH THE MEAL CATEGORY VARIABLE
+    fetch(`https:www.themealdb.com/api/json/v1/1/filter.php?c=${mealCategory}`) // gets the meals from the category determined above
+    .then(response => response.json())
+    .then(data => {
+        randomMealId = data.meals[Math.floor(Math.random() * data.meals.length)].idMeal // gets a random meal from that category
+
+        fetch(`https:www.themealdb.com/api/json/v1/1/lookup.php?i=${randomMealId}`) // uses the random meal id to get that meal from the API
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let meal = data.meals[0]
+            let ingredients = []
+            let measurementsArr = []
+        
+            for(const key in meal) { // for loops that grab the ingredients and measurements of the meal to display
+                for(let i = 1; i <= 20; i++) {
+                    if(key === `strIngredient${i}` && meal[key] != "" && meal[key] != null && meal[key] != " ") {
+                        ingredients.push(meal[key])
+                    }else if(key === `strMeasure${i}` && meal[key] != "" && meal[key] != null && meal[key] != " "){
+                        measurementsArr.push(meal[key])
+                    }
+                }
+            }
+
+            recipeName.innerHTML = data.meals[0].strMeal
+            ingredientsElement.innerHTML = ingredients
+            measurementsElement.innerHTML = data.meals[0].strMeasure1
+            recipeBulk.innerHTML = data.meals[0].strInstructions
+    
+            mealTitleElement.append(recipeName)
+            recipeMeasurementsElement.append(measurementsArr)
+            mealIngredientsElement.append(ingredients)
+            recipeInstructionElement.append(recipeBulk)
+        
+        })
+    })
+    
+
+}
+
 // END GAME FUNCTION
 export function endGame() {
+    showRecipe()
     retryButtonElement.addEventListener("click", reloadPage) // reloads the page when button is clicked
     endGameSectionElm.style.display = "flex" // displays end game section
     gameSection.style.display = "none" // hides main game section
@@ -52,92 +121,3 @@ export function endGame() {
     correctAnswerElement.innerHTML = "correct answers: " + correctAnswers // correct answers element
     incorrectAnswerElement.innerHTML = "incorrect answers: " + incorrectAnswers // incorrect answers element
 }
-
-
-// the API
-
-function showRecipe(country) {
-    var textPopUp = document.getElementById("random-recepie-title")
-    var textPopIng = document.getElementById("random-recepie-ing")
-    var textPopMes = document.getElementById("random-recepie-mes")
-    var bulkText = document.getElementById("howToDo")
-
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php?key=1')
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            // Use the console to examine the response
-            console.log(data);
-
-            var bearStuffie = []
-            bearStuffie.push(data.meals[0].strIngredient1)
-            bearStuffie.push(data.meals[0].strIngredient2)
-            bearStuffie.push(data.meals[0].strIngredient3)
-            bearStuffie.push(data.meals[0].strIngredient4)
-            bearStuffie.push(data.meals[0].strIngredient5)
-            bearStuffie.push(data.meals[0].strIngredient6)
-            bearStuffie.push(data.meals[0].strIngredient7)
-            bearStuffie.push(data.meals[0].strIngredient8)
-            bearStuffie.push(data.meals[0].strIngredient9)
-            bearStuffie.push(data.meals[0].strIngredient10)
-            bearStuffie.push(data.meals[0].strIngredient11)
-            bearStuffie.push(data.meals[0].strIngredient12)
-            bearStuffie.push(data.meals[0].strIngredient13)
-            bearStuffie.push(data.meals[0].strIngredient14)
-            bearStuffie.push(data.meals[0].strIngredient15)
-            bearStuffie.push(data.meals[0].strIngredient16)
-            bearStuffie.push(data.meals[0].strIngredient17)
-            bearStuffie.push(data.meals[0].strIngredient18)
-            bearStuffie.push(data.meals[0].strIngredient19)
-            bearStuffie.push(data.meals[0].strIngredient20)
-
-
-            // const rawDataJSON = (data);
-
-            var title = document.createElement("h4");
-            var ingredients = document.createElement("h4");
-            var recipeBulk = document.createElement("h5");
-            var measurements = document.createElement("h6");
-
-            var ingredientsToStr = "";
-            for (let i = 0; i <= bearStuffie.length; i++) {
-                if (bearStuffie[i] !== "" && bearStuffie[i] !== " " && bearStuffie[i] !== null && bearStuffie[i] && undefined) {
-
-                    ingredientsToStr += bearStuffie[i] + ", "
-                }
-            }
-            console.log(ingredientsToStr)
-
-            title.innerHTML = data.meals[0].strMeal;
-            ingredients.innerHTML = ingredientsToStr;
-            measurements.innerHTML = data.meals[0].strMeasure1;
-            recipeBulk.innerHTML = data.meals[0].strInstructions;
-
-            textPopUp.append(title);
-            textPopIng.append(ingredients);
-            textPopMes.append(measurements);
-            bulkText.append(recipeBulk);
-        })
-}
-
-showRecipe()
-
-
-// THE CODE FROM Q
-// fetch("https://www.themealdb.com/api/json/v1/1/random.php?key=1")
-// .then(response => response.json())
-// .then(data => {
-//     meal = data.meals[0];
-//     var ingredients = [];
-
-//     for(const key in meal) {
-//         for(var i = 1; i <= 20; i++) {
-//             if(key === `strIngredient${i}` && meal[key] != "" && meal[key] != null && meal[key] != " ") {
-//                 ingredients.push(meal[key]);
-//             }
-//         }
-//     }
-
-//     console.log(ingredients);
-// });
